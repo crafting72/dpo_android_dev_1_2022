cc9a33b241387dccb55950355213f9a968f79daa
crafting72 <matvey.revnivyh@gmail.com>
2024-10-18 20:19:05 +0500

    Homework is done. We have studied the project's resources.

---
+++ m5_quiz_resources/app/src/main/java/com/example/mindMeld/quiz/Question.kt
@@ -0,0 +1,7 @@
+package com.example.mindMeld.quiz
+
+class Question(
+    val question:String,
+    val answers:List<String>,
+    val feedback:List<String>,
+)
---
+++ m5_quiz_resources/app/src/main/java/com/example/mindMeld/quiz/Quiz.kt
@@ -0,0 +1,5 @@
+package com.example.mindMeld.quiz
+
+interface Quiz {
+    val questions: List<Question>
+}

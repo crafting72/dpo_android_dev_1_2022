faedb3500a6be601ed01c6bb06fcbf72058757f4
crafting72 <matvey.revnivyh@gmail.com>
2024-10-19 21:44:43 +0500

    Homework is done. We have learned how to localize our application into other languages and work with DataPicker.

---
+++ m9_quiz_localization/app/src/main/java/com/example/mindMeld/ui/main/FirstFragment.kt
@@ -0,0 +1,70 @@
+package com.example.mindMeld.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.mindMeld.R
+import com.example.mindMeld.databinding.FragmentFirstBinding
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val calendar = Calendar.getInstance()
+    private val dateFormat = SimpleDateFormat("dd.MM.yy")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        binding.dataPickerButton.setOnClickListener {
+            val constraints = CalendarConstraints.Builder()
+                .setOpenAt(calendar.timeInMillis)
+                .build()
+
+            val dateDialog = MaterialDatePicker.Builder.datePicker()
+                .setCalendarConstraints(constraints)
+                .setTitleText(resources.getString(R.string.yourBirthday))
+                .build()
+            dateDialog.addOnPositiveButtonClickListener { timeInMillis ->
+                calendar.timeInMillis = timeInMillis
+                Snackbar.make(binding.dataPickerButton,
+                    dateFormat.format(calendar.time),
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+            dateDialog.show(parentFragmentManager, "DatePicker")
+        }
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextButton.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ m9_quiz_localization/app/src/main/java/com/example/mindMeld/ui/main/SecondFragment.kt
@@ -0,0 +1,81 @@
+package com.example.mindMeld.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.mindMeld.R
+import com.example.mindMeld.databinding.FragmentSecondBinding
+import com.example.mindMeld.quiz.QuizStorage
+import java.util.Locale
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private val quizStorage = if (Locale.getDefault().language == "ru")
+        QuizStorage.getQuiz(QuizStorage.Locale.Ru)
+    else QuizStorage.getQuiz(QuizStorage.Locale.En)
+    private val question = quizStorage.questions
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        binding.answer1.editText(question[0])
+        binding.answer2.editText(question[1])
+        binding.answer3.editText(question[2])
+
+        binding.answer1.setOnCheckedChangeListener()
+        binding.answer2.setOnCheckedChangeListener()
+        binding.answer3.setOnCheckedChangeListener()
+
+        anim(binding.answer1)
+        anim(binding.answer2)
+        anim(binding.answer3)
+        anim(binding.resultButton)
+
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        binding.resultButton.setOnClickListener {
+            if (binding.answer1.feedback == -1) binding.answer1.feedback = 0
+            if (binding.answer2.feedback == -1) binding.answer2.feedback = 3
+            if (binding.answer3.feedback == -1) binding.answer3.feedback = 3
+            findNavController().navigate(
+                SecondFragmentDirections.actionSecondFragmentToThirdFragment(
+                    QuizStorage.answer(quizStorage,
+                        listOf(binding.answer1.feedback,
+                            binding.answer2.feedback,
+                            binding.answer3.feedback))))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun anim(view: View){
+        view.alpha = 0f
+        view.animate().apply {
+            duration = 1000
+            alpha(1f)
+        }.start()
+    }
+}
